803b261c4ebc41c55daacd171b4092d209ccfcdf
GrteenGI <Grteen@Grteen.com>
2023-07-18 15:49:50 +0800

    add Tobyte for OverFlow and Pair

--- src/pair/pair.go
+++ src/pair/pair.go
@@ -1,6 +1,9 @@
 package pair
 
-import "GtBase/src/object"
+import (
+	"GtBase/src/object"
+	"encoding/binary"
+)
 
 // Pair is used as the record
 type Pair struct {
@@ -18,6 +21,19 @@ func (p *Pair) Value() object.Object {
 	return p.value
 }
 
+func (p *Pair) ToByte() []byte {
+	keyByte := p.key.ToByte()
+	valByte := p.value.ToByte()
+	ofByte := p.overFlow.ToByte()
+
+	result := make([]byte, len(keyByte)+len(valByte)+len(ofByte)+1)
+	result = append(result, keyByte...)
+	result = append(result, valByte...)
+	result = append(result, ofByte...)
+
+	return result
+}
+
 func CreatePair(key, value object.Object, flag int8, of OverFlow) *Pair {
 	return &Pair{key: key, value: value, flag: flag, overFlow: of}
 }
@@ -29,6 +45,19 @@ type OverFlow struct {
 
 func (of *OverFlow) OverFlowInfo() (int32, int32) {
 	return of.overFlowIndex, of.overFlowOffset
+}
+
+func (of *OverFlow) ToByte() []byte {
+	result := make([]byte, 0, 8)
+	idxByte := make([]byte, 4)
+	offByte := make([]byte, 4)
+
+	binary.PutVarint(idxByte, int64(of.overFlowIndex))
+	binary.PutVarint(offByte, int64(of.overFlowOffset))
+	result = append(result, idxByte...)
+	result = append(result, offByte...)
+
+	return result
 }
 
 func CreateOverFlow(idx, off int32) OverFlow {
