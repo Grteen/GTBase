5a9604152abfac82997b831cb1a3428cfe077f88
GrteenGI <Grteen@Grteen.com>
2023-07-19 10:25:10 +0800

    add Log function for log message

--- pkg/glog/glog.go
+++ pkg/glog/glog.go
@@ -1,8 +1,12 @@
 package glog
 
 import (
+	"fmt"
 	"log"
+	"runtime"
+	"strings"
 	"sync"
+	"time"
 )
 
 var once sync.Once
@@ -14,6 +18,19 @@ func Glog() *log.Logger {
 	})
 
 	return glog
+}
+
+func Log(format string, a ...any) {
+	_, file, line, ok := runtime.Caller(1)
+	if !ok {
+		Glog().Println("runtime.Caller failed")
+	}
+
+	var builder strings.Builder
+	builder.WriteString("%v %v:%v ")
+	builder.WriteString(format)
+
+	Glog().Println(fmt.Sprintf(builder.String(), time.Now(), file, line, a))
 }
 
 func InitGlog() *log.Logger {
