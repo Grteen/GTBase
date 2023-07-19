980e63b9f98853246e8e18e89f26ecb476243c0d
GrteenGI <Grteen@Grteen.com>
2023-07-19 10:54:21 +0800

    add glog.Error

--- pkg/glog/glog.go
+++ pkg/glog/glog.go
@@ -24,6 +24,19 @@ func Log(format string, a ...any) {
 	fmt.Println(fmt.Sprintf(builder.String(), time.Now(), file, line, a))
 }
 
+func Error(format string, a ...any) error {
+	_, file, line, ok := runtime.Caller(1)
+	if !ok {
+		fmt.Println("runtime.Caller failed")
+	}
+
+	var builder strings.Builder
+	builder.WriteString("%v:%v ")
+	builder.WriteString(format)
+
+	return fmt.Errorf(fmt.Sprintf(builder.String(), file, line, a))
+}
+
 func InitGlog() *log.Logger {
 	return log.Default()
 }
