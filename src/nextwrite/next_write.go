b49220bf978036820a9569a023606394a9543121
GrteenGI <Grteen@Grteen.com>
2023-07-19 13:41:16 +0800

    add NextWriteFactory

--- src/nextwrite/next_write.go
+++ src/nextwrite/next_write.go
@@ -1,5 +1,7 @@
 package nextset
 
+import "sync"
+
 // NextWrite tell the Next Write Command where to write
 type NextWrite struct {
 	pageIndex  int32
@@ -10,5 +12,22 @@ func (nw *NextWrite) NextSetInfo() (int32, int32) {
 	return nw.pageIndex, nw.pageOffset
 }
 
-type NextSetFactory struct {
+// NextWriteFactory assign CMN to all write command
+// and assign NextWrite to all Set command
+type NextWriteFactory struct {
+	// commandNumber int32
+	nextWrite NextWrite
+	nwLock    sync.Mutex
+}
+
+var instance *NextWriteFactory
+var once sync.Once
+
+func getNextWriteFactory() *NextWriteFactory {
+	once.Do(func() {
+		// TODO init it's nextWrite
+		instance = &NextWriteFactory{}
+	})
+
+	return instance
 }
