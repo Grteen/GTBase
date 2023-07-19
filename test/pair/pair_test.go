39aa160fbfb5381325e587d4a4e8242bc98e280e
GrteenGI <Grteen@Grteen.com>
2023-07-19 09:22:21 +0800

    add Pair.ToByte test

--- test/pair/pair_test.go
+++ test/pair/pair_test.go
@@ -3,25 +3,53 @@ package pair
 import (
 	"GtBase/src/object"
 	"GtBase/src/pair"
+	"GtBase/utils"
 	"encoding/binary"
 	"fmt"
 	"testing"
 )
 
 func TestPairToByte(t *testing.T) {
-	data := []struct {
-		key            string
-		val            string
-		flag           int8
-		overFlowIndex  int32
-		overFlowOffset int32
-		res            string
-	}{
-		{"1a", "1a", 0, 0, 30, "4 "},
-	}
+	data := createTestPairToByteData()
 
-	p := pair.CreatePair(object.CreateGtString(data[0].key), object.CreateGtString(data[0].val), data[0].flag, pair.CreateOverFlow(data[0].overFlowIndex, data[0].overFlowOffset))
-	fmt.Println((p.ToByte()))
+	for _, d := range data {
+		p := pair.CreatePair(d.key, d.val, d.flag, pair.CreateOverFlow(d.overFlowIndex, d.overFlowOffset))
+		if !utils.EqualByteSlice(p.ToByte(), d.res) {
+			t.Errorf("Pair.ToByte should got %v but got %v", d.res, p.ToByte())
+		}
+	}
+}
+
+type PairByteTest struct {
+	key            object.Object
+	val            object.Object
+	flag           int8
+	overFlowIndex  int32
+	overFlowOffset int32
+	res            []byte
+}
+
+func createTestPairToByteData() []PairByteTest {
+	data := []PairByteTest{
+		{object.CreateGtString("Hello World"), object.CreateGtString("Good World"), 0, 0, 30, make([]byte, 0)},
+		{object.CreateGtString("Key"), object.CreateGtString("Val"), 1, 0, 30, make([]byte, 0)},
+	}
+	data[0].res = append(data[0].res, []byte{71, 111, 111, 100, 32, 87, 111, 114, 108, 100}...)
+	data[0].res = append(data[0].res, []byte{10, 0, 0, 0}...)
+	data[0].res = append(data[0].res, []byte{72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100}...)
+	data[0].res = append(data[0].res, []byte{11, 0, 0, 0}...)
+	data[0].res = append(data[0].res, []byte{0, 0, 0, 0}...)
+	data[0].res = append(data[0].res, []byte{0, 0, 0, 0}...)
+	data[0].res = append(data[0].res, []byte{30, 0, 0, 0}...)
+
+	data[1].res = append(data[1].res, []byte{86, 97, 108}...)
+	data[1].res = append(data[1].res, []byte{3, 0, 0, 0}...)
+	data[1].res = append(data[1].res, []byte{75, 101, 121}...)
+	data[1].res = append(data[1].res, []byte{3, 0, 0, 0}...)
+	data[1].res = append(data[1].res, []byte{1, 0, 0, 0}...)
+	data[1].res = append(data[1].res, []byte{0, 0, 0, 0}...)
+	data[1].res = append(data[1].res, []byte{30, 0, 0, 0}...)
+	return data
 }
 
 func TestBinaryAppend(t *testing.T) {
