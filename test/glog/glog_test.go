9694978122f49dce7be53fd62b02c74e0a811965
GrteenGI <Grteen@Grteen.com>
2023-07-19 11:26:40 +0800

    add test for glog.Error

--- test/glog/glog_test.go
+++ test/glog/glog_test.go
@@ -2,6 +2,7 @@ package glog
 
 import (
 	"GtBase/pkg/glog"
+	"errors"
 	"testing"
 )
 
@@ -9,4 +10,10 @@ func TestLog(t *testing.T) {
 	glog.Log("this is a test")
 	var str string = "good"
 	glog.Log("this is a %v test", str)
+}
+
+func TestError(t *testing.T) {
+	glog.Log(glog.Error("this is a error").Error())
+	err := errors.New("New error")
+	glog.Log(glog.Error("error because of %v", err).Error())
 }
