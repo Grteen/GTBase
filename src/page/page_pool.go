46025122b01708fa423aaf053b9cda6687f722c7
GrteenGI <Grteen@Grteen.com>
2023-07-20 16:24:28 +0800

    minor change

--- src/page/page_pool.go
+++ src/page/page_pool.go
@@ -113,6 +113,7 @@ func readOnePageOfBytes(f *os.File, offset int64) ([]byte, error) {
 // 	return pg.flushPage()
 // }
 
+// also clean the page
 func (p *Page) FlushPage() error {
 	if !p.Dirty() {
 		return glog.Error("FlushPage don't need to flush because page%v not dirty", p.GetIndex())
