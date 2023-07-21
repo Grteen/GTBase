8915d42297c29b76e2920c4fec980b398083fc1c
GrteenGI <Grteen@Grteen.com>
2023-07-21 11:26:33 +0800

    minor change to TestWriteInPage

--- test/bucket/bucket_test.go
+++ test/bucket/bucket_test.go
@@ -48,7 +48,7 @@ func TestWriteInPage(t *testing.T) {
 		b := bucket.CreateBucket(bucket.CreateBucketHeader(d.firstHashValue, d.secondHashValue), d.idx, d.off)
 		b.WriteInPage()
 
-		pg, err := page.ReadPage(-(d.firstHashValue + 1))
+		pg, err := page.ReadPage(-b.BucketHeader().CalIndexOfBucketPage())
 		if err != nil {
 			t.Errorf(err.Error())
 		}
@@ -60,6 +60,37 @@ func TestWriteInPage(t *testing.T) {
 
 		if !utils.EqualByteSliceOnlyInMinLen(pg.Src(), got) {
 			t.Errorf("ReadPage should got %v but got %v", got, pg.Src()[:len(got)])
+		}
+	}
+
+	data = []struct {
+		firstHashValue  int32
+		secondHashValue int32
+		idx             int32
+		off             int32
+		res             []byte
+	}{
+		{1, 0, 0, 0, []byte{0, 0, 0, 0, 0, 0, 0, 0}},
+		{10, 0, 1, 26, []byte{1, 0, 0, 0, 26, 0, 0, 0}},
+		{20, 0, 2, 257, []byte{2, 0, 0, 0, 1, 1, 0, 0}},
+	}
+
+	for i := 0; i < len(data); i++ {
+		d := data[i]
+
+		b := bucket.CreateBucket(bucket.CreateBucketHeader(d.firstHashValue, d.secondHashValue), d.idx, d.off)
+		b.WriteInPage()
+
+		pg, err := page.ReadPage(-b.BucketHeader().CalIndexOfBucketPage())
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+
+		s := b.BucketHeader().CalOffsetOfBucketPage()
+		e := s + int32(len(b.ToByte()))
+
+		if !utils.EqualByteSlice(pg.Src()[s:e], b.ToByte()) {
+			t.Errorf("ReadPage should got %v but got %v", b.ToByte(), pg.Src()[s:e])
 		}
 	}
 }
