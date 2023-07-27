51eff0df2b85136ca45235b509c5f3dcc2863b98
Grteen <gyjgrteen@gmail.com>
2023-07-27 01:17:26 +0000

    fix del bug

---
+++ src/client/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:1234")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter text: ")
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSuffix(text, "\n")
+
+		_, err := conn.Write([]byte(text))
+		if err != nil {
+			fmt.Println("Error sending:", err)
+			return
+		}
+
+		buf := make([]byte, 1024)
+		_, err = conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("Response:", string(buf))
+	}
+}
