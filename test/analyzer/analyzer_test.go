7ac9890599a6999f272d0b336afb83bb3a772f3d
Grteen <gyjgrteen@gmail.com>
2023-07-28 05:07:43 +0000

    add Slave_Analyzer

--- test/analyzer/analyzer_test.go
+++ test/analyzer/analyzer_test.go
@@ -15,13 +15,63 @@ import (
 )
 
 func TestAnalyzer(t *testing.T) {
+	page.DeleteBucketPageFile()
+	page.InitBucketPageFile()
+	data := []struct {
+		key string
+		val string
+	}{
+		{"Key", "Val"},
+		{"Hello", "World"},
+	}
+
+	for _, d := range data {
+		cmd := make([][]byte, 0)
+		cmd = append(cmd, []byte(d.key))
+		cmd = append(cmd, []byte(d.val))
+
+		a := analyzer.CreateSetAnalyzer(cmd, []byte(""), -1)
+		res := a.Analyze().Exec().ToString()
+		if res != constants.ServerOkReturn {
+			t.Errorf("Exec should get %v but got %v", constants.ServerOkReturn, res)
+		}
+	}
+
+	for _, d := range data {
+		cmd := make([][]byte, 0)
+		cmd = append(cmd, []byte(d.key))
+
+		a := analyzer.CreateGetAnalyzer(cmd, []byte(""), -1)
+		res := a.Analyze().Exec().ToString()
+		if res != d.val {
+			t.Errorf("Exec should get %v but got %v", d.val, res)
+		}
+	}
+
+	cmd := make([][]byte, 0)
+	cmd = append(cmd, []byte(data[1].key))
+
+	a := analyzer.CreateDelAnalyzer(cmd, []byte(""), -1)
+	res := a.Analyze().Exec().ToString()
+	if res != constants.ServerOkReturn {
+		t.Errorf("Exec should get %v but got %v", constants.ServerOkReturn, res)
+	}
+
+	a2 := analyzer.CreateGetAnalyzer(cmd, []byte(""), -1)
+	res = a2.Analyze().Exec().ToString()
+	if res != constants.ServerGetNilReturn {
+		t.Errorf("Exec should get %v but got %v", constants.ServerGetNilReturn, res)
+	}
+}
+
+func TestSlave(t *testing.T) {
 	listenSock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		t.Fatalf("Failed to create socket: %v", err)
 	}
 	defer syscall.Close(listenSock)
 
-	addr := syscall.SockaddrInet4{Port: 4841}
+	addr := syscall.SockaddrInet4{Port: 7866}
 	copy(addr.Addr[:], net.ParseIP("127.0.0.1").To4())
 
 	err = syscall.Bind(listenSock, &addr)
@@ -38,7 +88,7 @@ func TestAnalyzer(t *testing.T) {
 
 	ch := make(chan []byte)
 	go func(result []byte) {
-		conn, err := net.Dial("tcp", "127.0.0.1:4841")
+		conn, err := net.Dial("tcp", "127.0.0.1:7866")
 		if err != nil {
 			t.Errorf(err.Error())
 			return
@@ -86,12 +136,13 @@ func TestAnalyzer(t *testing.T) {
 
 	rs := replic.CreateReplicState()
 	c := client.CreateGtBaseClient(nfd, client.CreateAddress("127.0.0.1", 0))
+
 	cmd := make([][]byte, 0)
 	cmd = append(cmd, utils.Encodeint32ToBytesSmallEnd(0))
 	cmd = append(cmd, utils.Encodeint32ToBytesSmallEnd(0))
 	cmd = append(cmd, utils.Encodeint32ToBytesSmallEnd(1))
 
-	analyzer.CreateSlaveAnalyzer(cmd, c, rs).Analyze().Exec().ToByte()
+	analyzer.CreateSlaveAnalyzer(cmd, c, rs).Analyze().Exec()
 
 	seqbts := <-ch
 	seq := utils.EncodeBytesSmallEndToint32(seqbts)
